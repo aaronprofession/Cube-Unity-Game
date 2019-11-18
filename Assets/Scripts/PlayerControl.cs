623d19d1d15cf97f4d5dcd864ef36cefb1fa6369
Aaron <aaronprofession@gmail.com>
2019-11-17 22:36:17 -0500

    Finished Movement and Collision scripts

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Public variables
+    public float speed;
+    
+    // Private variables
+    private Rigidbody2D rBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float horz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        rBody.velocity = new Vector2(horz * speed, vert * speed);
+
+    }
+}
