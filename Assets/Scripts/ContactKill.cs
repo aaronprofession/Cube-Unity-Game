80ffc3beb033355c98faa4c8881e9ae3eacc2b39
Aaron <aaronprofession@gmail.com>
2019-11-19 18:36:29 -0500

    Added Hazard Assets, Destroy script, Rotatory script, DeathBox and DeathSpinner Prefabs. and Finished half of planned object placements.

---
+++ Assets/Scripts/ContactKill.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContactKill : MonoBehaviour
+{
+    //Destroys touching object by contact
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(other.gameObject);     
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+    public float spinSpeed = 5;
+    public float spinMultiplier = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rBody.angularVelocity = spinSpeed * spinMultiplier;
+    }
+}
