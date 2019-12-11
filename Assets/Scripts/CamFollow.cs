b3787a32958f5ec0dba4141e9d18cd803e16a3e6
Aaron <aaronprofession@gmail.com>
2019-12-10 20:27:46 -0500

    Fixed timer on reset, Added music and music credit

--- Assets/Scripts/CamFollow.cs
+++ Assets/Scripts/CamFollow.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class CamFollow : MonoBehaviour
-{ 
-    public GameObject Player;
+{
+    
+    public GameObject Player, Tutorial;
+    float TutorialTime = 0;
     // Start is called before the first frame update
     public static bool isDead = false;
-
+    private void Start()
+    {
+        isDead = false;
+    }
     public Text timerLabel;
     // Update is called once per frame
     void Update()
@@ -32,6 +37,11 @@ public class CamFollow : MonoBehaviour
                 SceneManager.LoadScene("Level");
             }
         }
+        if(TutorialTime > 2)
+        {
+            Tutorial.SetActive(false);
+        }
+        
 
     }
     private float time;
@@ -41,13 +51,14 @@ public class CamFollow : MonoBehaviour
         if(isDead == false)
         {
             time += Time.deltaTime;
-
+            TutorialTime = time;
             var minutes = time / 60;
             var seconds = time % 60;
             var fractionOfSeconds = (time * 100) % 100;
-
+            
 
             timerLabel.text = string.Format("{0:00} : {1:00} : {2:000}", minutes, seconds, fractionOfSeconds);
+            
         }
         
     }
--- Assets/Scripts/ContactKill.cs
+++ Assets/Scripts/ContactKill.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class ContactKill : MonoBehaviour
 {
     public GameObject DeathText;
-    
+    public AudioSource Music, failureMusic;
     //Destroys touching object by contact
     void OnCollisionEnter2D(Collision2D other)
     {
         Destroy(other.gameObject);
         DeathText.gameObject.SetActive(true);
         CamFollow.isDead = true;
+        Music.Stop();
+        failureMusic.Play();
     }
 }
